9dfb048eba0262cddf958ed12173f88dd6b36f04
RayanceKing <88320297+RayanceKing@users.noreply.github.com>
2024-12-26 22:44:46 +0800

    new file:   .gitignore 	new file:   LICENSE 	new file:   QQBot/mcdreforged.plugin.json 	new file:   QQBot/qq_bot/Config.py 	new file:   QQBot/qq_bot/Core.py 	new file:   QQBot/qq_bot/Utils.py 	new file:   QQBot/qq_bot/Webscoket/Base.py 	new file:   QQBot/qq_bot/Webscoket/Listener.py 	new file:   QQBot/qq_bot/Webscoket/Sender.py 	new file:   QQBot/qq_bot/Webscoket/__init__.py 	new file:   QQBot/qq_bot/__init__.py 	new file:   README.md 	new file:   mcdreforged.plugin.json 	new file:   requirements.txt

---
+++ QQBot/qq_bot/Config.py
@@ -0,0 +1,14 @@
+from mcdreforged.api.utils import Serializable
+
+
+class Config(Serializable):
+    uri: str = 'ws://127.0.0.1:8000/'
+    # 服务器名称
+    name: str = 'name'
+    # 和机器人服务器的 token 一致
+    token: str = 'YourToken'
+    # 尝试重连间隔(s)
+    reconnect_interval: int = 5
+
+    # 无需管，同 .env 里的 SYNC_ALL_GAME_MESSAGE
+    flag: bool = False
---
+++ QQBot/qq_bot/Webscoket/Listener.py
@@ -0,0 +1,82 @@
+import time
+from json import JSONDecodeError, dumps
+from threading import Thread
+
+from mcdreforged.api.event import LiteralEvent
+from mcdreforged.api.types import PluginServerInterface
+from psutil import Process
+from websocket import WebSocketConnectionClosedException
+
+from .Base import Websocket
+from ..Config import Config
+from ..Utils import decode, encode
+
+
+class WebsocketListener(Websocket, Thread):
+    flag: bool = True
+    process: Process = None
+
+    def __init__(self, server: PluginServerInterface, config: Config):
+        Thread.__init__(self, name='WebsocketListener', daemon=True)
+        Websocket.__init__(self, server, config, 'minecraft')
+
+    def run(self):
+        while self.flag:
+            if self.connect():
+                self.server.logger.info('与机器人的连接已建立！已通知插件。')
+                self.server.dispatch_event(LiteralEvent('qq_bot.websocket_connected'), (None, None))
+                try:
+                    while True:
+                        response = None
+                        data = decode(self.websocket.recv())
+                        self.server.logger.info(F'收到来自机器人的消息 {data}')
+                        event_type = data.get('type')
+                        data = data.get('data')
+                        if event_type == 'command':
+                            response = self.execute_command(data)
+                        elif event_type == 'mcdr_command':
+                            response = self.execute_mcdr_command(data)
+                        elif event_type == 'player_list':
+                            response = self.get_player_list(data)
+                        elif event_type == 'server_occupation':
+                            response = self.get_server_occupation()
+                        elif event_type == 'message':
+                            self.server.execute(F'tellraw @a {dumps(data)}')
+                            continue
+                        if response is not None:
+                            self.server.logger.debug(F'向机器人发送消息 {response}')
+                            self.websocket.send(encode({'success': True, 'data': response}))
+                            continue
+                        self.server.logger.warning(F'无法解析的消息！')
+                        self.websocket.send(encode({'success': False}))
+                except (WebSocketConnectionClosedException, JSONDecodeError, ConnectionError):
+                    self.server.logger.warning('与机器人的连接已断开！')
+                    self.server.dispatch_event(LiteralEvent('qq_bot.websocket_closed'), (None, None))
+            time.sleep(self.config.reconnect_interval)
+
+    def execute_command(self, command: str):
+        if self.server.is_rcon_running():
+            return self.server.rcon_query(command)
+        self.server.execute(command)
+        return '命令已发送，但由于 Rcon 未连接无返回值。'
+
+    def execute_mcdr_command(self, command: str):
+        self.server.execute_command(command)
+        return {}
+
+    def get_player_list(self, data: dict):
+        if not self.server.is_rcon_running():
+            self.server.logger.warning('Rcon 未连接，无法获取玩家列表。')
+            return None
+        players = self.server.rcon_query('list')
+        players = players.replace(' ', '')
+        if len(players := players.split(':')) == 2:
+            return players[1].split(',') if players[1] else []
+        return []
+
+    def get_server_occupation(self):
+        if self.process is not None:
+            cpu = self.process.cpu_percent()
+            ram = self.process.memory_percent()
+            return cpu, ram
+        return False
